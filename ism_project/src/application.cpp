f4f3ec72ad9d97d58d66b99d5ad1db89c46d5008
AnaelleW <anaellewiersch@gmail.com>
2025-10-13 15:26:10 +0200

    V2_appli

---
+++ ism_project/src/application.cpp
@@ -0,0 +1,58 @@
+#include "application.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <Arduino.h>
+
+static state_motor_t state_motor;
+
+static cfg_moteur M1 = {34, 35, 12};
+static cfg_moteur M2 = {};
+static uint16_t timeout;
+static uint16_t tps_debut;
+
+
+bool stop(){
+    return controler_moteur(M1, 0) && controler_moteur(M2, 0);
+}
+
+bool forward(uint16_t tension){
+    return controler_moteur(M1, tension) && controler_moteur(M2, -tension);
+}
+
+bool turn(uint16_t tension){
+    return controler_moteur(M1, tension);
+}
+
+bool existe_message(){
+    return (Serial.available() > 0);
+}
+
+void on_dt_event(){
+    switch (state_motor){
+        case IDLE:
+            if (existe_message()) state_motor = RECEIVING;
+        break;
+        case RECEIVING:
+            message_t msg = receive();
+            timeout = msg.timeout;
+            tps_debut = millis();
+            if (msg.code == 0) state_motor = STOP;
+            if (msg.code == 1) state_motor = FORWARD;
+            if (msg.code == 2) state_motor = TURN;
+        break;
+        case STOP:
+            stop();
+            if(existe_message()) state_motor = RECEIVING;
+        break;
+        case FORWARD:
+            forward(msg.tension);
+            if(millis()-tps_debut > timeout) state_motor = IDLE;
+            if(existe_message()) state_motor = RECEIVING;
+        break;
+        case TURN:
+            turn(msg.tension);
+            if(millis()-tps_debut > timeout) state_motor = IDLE;
+            if(existe_message()) state_motor = RECEIVING;
+        break;
+    } 
+}
--- ism_project/src/main.cpp
+++ ism_project/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include "application.h"
+#include "communication.h"
+#include "motor.h"
 
 void setup() {
 
 }
 
 void loop() {
-  
+    on_dt_event();
 }
