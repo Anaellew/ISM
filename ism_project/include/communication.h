0733c65f53a4fa073772231e59678c7c7d707cd7
SachaHsn <64612224+SachaHsn@users.noreply.github.com>
2025-10-13 16:58:38 +0200

    comm.h à jour

--- ism_project/include/communication.h
+++ ism_project/include/communication.h
@@ -6,11 +6,17 @@
 typedef struct message_t{
     uint16_t code;
     uint16_t timeout;
-    uint16_t tension;
+    uint16_t voltage;
 } message_t;
+
+int16_t code_stop = 0;
+int16_t code_forward = 1;
+int16_t code_turn = 2;
 
 message_t receive();
 
 bool send(message_t msg);
+
+bool compare_code(char str1[24], char str2[24]);
 
 #endif
