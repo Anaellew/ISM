db7a5eae0e4d928267a6d7a2f5735009137e101f
Constantin Clauzure <constantin.clauzure@gmail.com>
2025-10-13 17:13:22 +0200

    MoteurV2

--- ism_project/include/motor.h
+++ ism_project/include/motor.h
@@ -1,4 +1,14 @@
-typedef struct motor
-{
-    __UINT16_TYPE__ 
-};
+#ifndef MOTOR
+#define MOTOR
+
+typedef struct cfg_motor {
+		uint16_t BI1;
+		uint16_t BI2;
+		uint16_t PWM;
+	} cfg_motor;
+
+bool control_motor(cfg_motor M, int16_t voltage);
+
+void motor_coast(cfg_motor M);
+
+#endif 
